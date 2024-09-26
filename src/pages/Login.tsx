76ec503ba88495e6f897ce30a9269d3f0e641e38
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-09-27 01:47:40 +0300

    feat: add login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,40 @@
+import Button from "../components/ui/Button";
+import logo from "../assets/images/logos/logo.png";
+import Input from "../components/ui/Input";
+
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center h-screen px-8 py-4 bg-white xs:py-12">
+      <div className="flex flex-col justify-around min-h-screen space-y-6">
+        <div className="flex items-center justify-center">
+          <img className="w-48 xs:w-56" src={logo} alt="Company Logo" />
+        </div>
+        <div>
+          <div className="mb-10 text-3xl text-center xs:mb-16 xs:text-4xl text-primary">
+            Login
+          </div>
+          <form className="flex flex-col space-y-8">
+            <div>
+              <Input label="Username" name="username" type="text" />
+            </div>
+            <div>
+              <Input label="Password" name="password" type="password" />
+            </div>
+
+            <Button
+              label="Login"
+              color="mx-1 bg-primary text-white"
+              shadowColor="rgba(255,255,255, 0.2)"
+            />
+          </form>
+          <div className="mt-5 text-[9px] xs:text-[10px] space-x-2 xs:space-x-6 ">
+            <span>Don't you have an account?</span>
+            <a className="text-purple-400" href="#">
+              Sign up
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
