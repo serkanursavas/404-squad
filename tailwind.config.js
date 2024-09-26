57e7822d866229344df9fc45dc4d298d8a9104c4
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-09-27 01:46:42 +0300

    feat: add xs screen config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
     extend: {
       screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px'
+        xs: "430px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
       },
       colors: {
         primary: {
-          DEFAULT: '#04764E' // Ana renk
+          DEFAULT: "#04764E", // Ana renk
         },
-        secondary: '#F2C94C', // İkincil renk (örn: vurgu, butonlar)
-        accent: '#F25C05', // Canlı vurgu renkleri (örn: CTA butonları)
+        secondary: "#F2C94C", // İkincil renk (örn: vurgu, butonlar)
+        accent: "#F25C05", // Canlı vurgu renkleri (örn: CTA butonları)
         neutral: {
-          light: '#F7F8FA', // Arka plan rengi
-          DEFAULT: '#E5E7EB', // Varsayılan nötr
-          dark: '#4B5563' // Metin ya da sınırlar için koyu nötr
+          light: "#F7F8FA", // Arka plan rengi
+          DEFAULT: "#E5E7EB", // Varsayılan nötr
+          dark: "#4B5563", // Metin ya da sınırlar için koyu nötr
         },
-        error: '#D32F2F',
-        success: '#28a745'
+        error: "#D32F2F",
+        success: "#28a745",
       },
       boxShadow: {
-        'custom-light': '0 2px 10px rgba(0, 0, 0, 0.1)', // Hafif gölge
-        'custom-dark': '0 4px 14px rgba(0, 0, 0, 0.2)', // Daha belirgin gölge
-        pixel: '4px 4px 0px 0px #000', // Piksel tarzı gölge
-        'pixel-light': '4px 4px 0px 0px #4B5563', // Piksel tarzı gölge
-        light: '2px 2px 14px 2px #4B5563', // Piksel tarzı gölge
-        x2light: '2px 2px 8px 1px #4B5563' // Piksel tarzı gölge
+        "custom-light": "0 2px 10px rgba(0, 0, 0, 0.1)", // Hafif gölge
+        "custom-dark": "0 4px 14px rgba(0, 0, 0, 0.2)", // Daha belirgin gölge
+        pixel: "4px 4px 0px 0px #000", // Piksel tarzı gölge
+        "pixel-light": "4px 4px 0px 0px #4B5563", // Piksel tarzı gölge
+        light: "2px 2px 14px 2px #4B5563", // Piksel tarzı gölge
+        x2light: "2px 2px 8px 1px #4B5563", // Piksel tarzı gölge
       },
       fontFamily: {
-        sans: ['"Press Start 2P"', 'sans-serif'],
-        retro: ['VT323', 'monospace'], // Retro terminal tarzı
-        body: ['"Press Start 2P"', 'sans-serif'] // Ana yazı tipi
-      }
+        sans: ['"Press Start 2P"', "sans-serif"],
+        retro: ["VT323", "monospace"], // Retro terminal tarzı
+        body: ['"Press Start 2P"', "sans-serif"], // Ana yazı tipi
+      },
       // keyframes: {
       //   pixelShadow: {
       //     '0%, 100%': {
@@ -50,7 +51,7 @@ export default {
       // animation: {
       //   pixelShadow: 'pixelShadow 1.5s infinite' // Pixel gölge animasyonu
       // }
-    }
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
