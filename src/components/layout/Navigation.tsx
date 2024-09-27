6d975c0073de6c44388f51199c8eb1c4579debd3
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-09-27 04:38:20 +0300

    feat:players added to menu

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,26 +1,28 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
-import homeIcon from '../../assets/icons/home.svg'
-import matchesIcon from '../../assets/icons/matches.svg'
-import profileIcon from '../../assets/icons/profile.svg'
+import homeIcon from "../../assets/icons/home.svg";
+import matchesIcon from "../../assets/icons/matches.svg";
+import profileIcon from "../../assets/icons/profile.svg";
+import playersIcon from "../../assets/icons/players.svg";
 
-import Icons from '../ui/Icons'
+import Icons from "../ui/Icons";
 
 interface Props {
-  toggleMobileMenu: () => void
+  toggleMobileMenu: () => void;
 }
 
 const navLinks = [
-  { to: '/', label: 'Home', icon: homeIcon },
-  { to: '/matches', label: 'Matches', icon: matchesIcon },
-  { to: '/profile/1', label: 'Profile', icon: profileIcon }
-]
+  { to: "/", label: "Home", icon: homeIcon },
+  { to: "/matches", label: "Matches", icon: matchesIcon },
+  { to: "/profile/1", label: "Profile", icon: profileIcon },
+  { to: "/players", label: "Players", icon: playersIcon },
+];
 
 export default function Navigation({ toggleMobileMenu }: Props) {
   return (
     <div className="absolute top-0 left-0 flex flex-col items-center justify-center w-screen h-full text-3xl bg-primary">
       <ul className="space-y-10">
-        {navLinks.map(navLink => {
+        {navLinks.map((navLink) => {
           return (
             <li key={navLink.to}>
               <NavLink
@@ -28,19 +30,16 @@ export default function Navigation({ toggleMobileMenu }: Props) {
                 onClick={toggleMobileMenu}
                 className="flex items-center space-x-2 "
                 style={({ isActive }) => ({
-                  color: isActive ? 'white' : undefined
+                  color: isActive ? "white" : undefined,
                 })}
               >
-                <Icons
-                  src={navLink.icon}
-                  className="h-full w-9"
-                />
+                <Icons src={navLink.icon} className="h-full w-9" />
                 <span>{navLink.label}</span>
               </NavLink>
             </li>
-          )
+          );
         })}
       </ul>
     </div>
-  )
+  );
 }
