76bc417bc0d29c99946eb934aad08693638f5efb
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-09-29 00:44:49 +0300

    feat: footer added

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,22 +1,27 @@
-import { Outlet } from 'react-router-dom'
-import Header from '../components/layout/Header'
-import Navigation from '../components/layout/Navigation'
-import { useState } from 'react'
+import { Outlet } from "react-router-dom";
+import Header from "../components/layout/Header";
+import Navigation from "../components/layout/Navigation";
+import { useState } from "react";
+import Footer from "../components/layout/Footer";
 
 export default function MainLayout() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleMobileMenu = () => {
-    setIsOpen(prevIsOpen => !prevIsOpen)
-  }
+    setIsOpen((prevIsOpen) => !prevIsOpen);
+  };
 
   return (
     <div className="bg-neutral">
-      <Header
-        toggleMobileMenu={toggleMobileMenu}
-        isOpen={isOpen}
-      />
-      <main className="p-2">{isOpen ? <Navigation toggleMobileMenu={toggleMobileMenu} /> : <Outlet />}</main>
+      <Header toggleMobileMenu={toggleMobileMenu} isOpen={isOpen} />
+      <main className="p-2">
+        {isOpen ? (
+          <Navigation toggleMobileMenu={toggleMobileMenu} />
+        ) : (
+          <Outlet />
+        )}
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
