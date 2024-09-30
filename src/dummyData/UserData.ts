057e4b0cf0bdd0f1c9f764239cfdbcceca5dab07
beyza Bolattekin <beyza@192.168.1.9>
2024-09-30 22:48:21 +0300

    feat: crated seperate files for dummy data

---
+++ src/dummyData/UserData.ts
@@ -0,0 +1,34 @@
+import { UserInfo } from "../types/UserTypes";
+
+export const dummyUserInfo: UserInfo[] = [
+  {
+    id: 1,
+    username: "Serkan",
+    role: "admin",
+  },
+  {
+    id: 2,
+    username: "Metehan",
+    role: "user",
+  },
+  {
+    id: 3,
+    username: "Caner",
+    role: "user",
+  },
+  {
+    id: 4,
+    username: "Isa Can",
+    role: "admin",
+  },
+  {
+    id: 5,
+    username: "Hayri Sencer",
+    role: "user",
+  },
+  {
+    id: 6,
+    username: "Ahmet Furkan",
+    role: "user",
+  },
+];
