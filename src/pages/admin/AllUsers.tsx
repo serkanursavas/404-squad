7b9db7e51dbf74d648117812cc72929fc9c46d84
beyza Bolattekin <beyza@192.168.1.9>
2024-09-30 22:52:56 +0300

    refac: dummy data moved

--- src/pages/admin/AllUsers.tsx
+++ src/pages/admin/AllUsers.tsx
@@ -1,26 +1,9 @@
 import AllUsersList from "../../components/admin/AllUsersList";
-
-const dummyUserInfo = [
-  {
-    id: 1,
-    userName: "Serkan",
-    role: "admin",
-  },
-  {
-    id: 2,
-    userName: "Metehan",
-    role: "user",
-  },
-  {
-    id: 3,
-    userName: "Caner",
-    role: "user",
-  },
-];
+import { dummyUserInfo } from "../../dummyData/UserData";
 
 export default function AllUsers() {
   return (
-    <div className="space-y-6">
+    <div className="w-full">
       <AllUsersList usersData={dummyUserInfo} />
     </div>
   );
