e8eb65fbfea9f790b48640b4dac880288f1246f9
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-10-06 20:51:54 +0300

    fix: conflict again

--- src/components/admin/PlayerCard.tsx
+++ src/components/admin/PlayerCard.tsx
@@ -8,24 +8,24 @@ type PlayersListProps = {
 
 export default function PlayerCard({ player }: PlayersListProps) {
   return (
-    <tr className="text-sm  even:bg-neutral odd:bg-neutral-dark odd:text-white">
-      <td scope="col" className="border px-4 py-2">
+    <tr className="text-sm even:bg-neutral odd:bg-neutral-dark odd:text-white">
+      <td scope="col" className="px-4 py-2 border">
         {player.id}
       </td>
-      <td scope="col" className="border px-4 py-2">
+      <td scope="col" className="px-4 py-2 border">
         {player.name}
       </td>
-      <td scope="col" className="border px-4 py-2">
+      <td scope="col" className="px-4 py-2 border">
         {player.age}
       </td>
-      <td scope="col" className="border px-4 py-2">
+      <td scope="col" className="px-4 py-2 border">
         {player.position}
       </td>
-      <td scope="col" className="border px-4 py-2 flex justify-center gap-10">
+      <td scope="col" className="flex justify-center gap-10 px-4 py-2 border">
         <Link to={`/admin/update-player/${player.id}`}>
-          <Button label="Update" color="bg-primary text-white text-xs" />
+          <Button label="Update" className="text-xs text-white bg-primary" />
         </Link>
-        <Button label="Delete" color="bg-primary text-white text-xs" />
+        <Button label="Delete" className="text-xs text-white bg-primary" />
       </td>
     </tr>
   );
