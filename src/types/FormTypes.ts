0e5830a15b19769384675e067120b7b95865e1d4
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-10-08 21:23:55 +0300

    refac: type folders extentet

--- src/types/FormTypes.ts
+++ src/types/FormTypes.ts
@@ -1,8 +1,23 @@
 export interface SignupFormValues {
-  username: string
-  password: string
-  name: string
-  surname: string
-  position: string
-  foot: string
+  username: string;
+  password: string;
+  name: string;
+  surname: string;
+  position: string;
+  foot: string;
+}
+
+export interface SelectOption {
+  value: string;
+  label: string;
+}
+
+export interface CreateMatchFormData {
+  id: number | null;
+  location: string;
+  matchDate: string;
+  teamSize: number;
+  whiteTeam: String[];
+  blackTeam: String[];
+  isPlayed: boolean;
 }
