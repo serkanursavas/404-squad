a9fa8ade848394c743167165d944cef28d39231c
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-10-08 21:20:47 +0300

    refac: some properties added to components

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,6 +6,7 @@ type ButtonProps = {
   shadowColor?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
+  type?: "button" | "submit" | "reset";
 };
 
 export default function Button({
@@ -14,15 +15,15 @@ export default function Button({
   shadowColor = "grey",
   onClick,
   disabled = false,
+  type,
 }: ButtonProps): JSX.Element {
   return (
     <button
-      className={`relative px-3 py-2 ${className} btn text-[12px] ${
-        disabled ? "opacity-50 cursor-not-allowed" : ""
-      }`}
+      className={`relative px-3 py-2 ${className} btn text-[12px] ${disabled ? "opacity-50 cursor-not-allowed" : ""}`}
       style={{ "--shadow-color": shadowColor } as React.CSSProperties}
       onClick={onClick}
       disabled={disabled}
+      type={type}
     >
       {label}
     </button>
