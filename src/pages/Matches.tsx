518d77a8609cc21b3d97e3968192fc1d92a3af83
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-10-08 21:16:17 +0300

    refac: cardwrapper addit to matchcard component and used

--- src/pages/Matches.tsx
+++ src/pages/Matches.tsx
@@ -1,76 +1,76 @@
-import MatchesList from '../components/match/MatchesList'
+import MatchesList from "../components/match/MatchesList";
 
-import blackLogo from '../assets/images/club-black.svg'
-import whiteLogo from '../assets/images/club-white.svg'
-import TypingEffect from '../components/ui/TypingEffect'
+import blackLogo from "../assets/images/club-black.svg";
+import whiteLogo from "../assets/images/club-white.svg";
+import TypingEffect from "../components/ui/TypingEffect";
 
 const dummyMatchInfo = [
   {
     id: 1,
-    date: 'Monday, 24 September',
+    date: "Monday, 24 September",
     team1: {
-      name: 'Black',
-      logo: blackLogo
+      name: "Black",
+      logo: blackLogo,
     },
     score1: 4,
     team2: {
-      name: 'White',
-      logo: whiteLogo
+      name: "White",
+      logo: whiteLogo,
     },
-    score2: 6
+    score2: 6,
   },
   {
     id: 2,
-    date: 'Wednesday, 11 July',
+    date: "Wednesday, 11 July",
     team1: {
-      name: 'White',
-      logo: whiteLogo
+      name: "White",
+      logo: whiteLogo,
     },
     score1: 2,
     team2: {
-      name: 'Black',
-      logo: blackLogo
+      name: "Black",
+      logo: blackLogo,
     },
-    score2: 9
+    score2: 9,
   },
   {
     id: 3,
-    date: 'Monday, 24 September',
+    date: "Monday, 24 September",
     team1: {
-      name: 'Black',
-      logo: blackLogo
+      name: "Black",
+      logo: blackLogo,
     },
     score1: 4,
     team2: {
-      name: 'White',
-      logo: whiteLogo
+      name: "White",
+      logo: whiteLogo,
     },
-    score2: 6
+    score2: 6,
   },
   {
     id: 4,
-    date: 'Tuesday, 4 May',
+    date: "Tuesday, 4 May",
     team1: {
-      name: 'Black',
-      logo: blackLogo
+      name: "Black",
+      logo: blackLogo,
     },
     score1: 9,
     team2: {
-      name: 'White',
-      logo: whiteLogo
+      name: "White",
+      logo: whiteLogo,
     },
-    score2: 11
-  }
-]
+    score2: 11,
+  },
+];
 
 export default function Matches() {
   return (
     <div className="space-y-6">
       <TypingEffect
-        text={['Latest Matches']}
+        text={["Latest Matches"]}
         className="text-sm text-purple-400 "
       />
       <MatchesList matchesData={dummyMatchInfo} />
     </div>
-  )
+  );
 }
