d1e8b69a2869002c2cc0354c509b2d79561e72a0
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-10-09 20:49:06 +0300

    refac: delete button changed

--- src/components/admin/AllUsersList.tsx
+++ src/components/admin/AllUsersList.tsx
@@ -7,28 +7,45 @@ type UsersListProps = {
 
 export default function AllUsersList({ usersData }: UsersListProps) {
   return (
-    <div className=" text-center md:w-10/12 mx-auto mt-3">
+    <div className="mx-auto mt-3 text-center md:w-10/12">
       <h1 className="text-2xl text-neutral-dark ">Manage Users</h1>
-      <table className="table-auto w-full border-separate mt-4">
-        <thead className="text-primary text-base">
+      <table className="w-full mt-4 border-separate table-auto">
+        <thead className="text-base text-primary">
           <tr>
-            <th scope="col" className="border px-4 py-2">
+            <th
+              scope="col"
+              className="px-4 py-2 border"
+            >
               Id
             </th>
-            <th scope="col" className="border px-4 py-2">
+            <th
+              scope="col"
+              className="px-4 py-2 border"
+            >
               Name
             </th>
-            <th scope="col" className="border px-4 py-2">
+            <th
+              scope="col"
+              className="px-4 py-2 border"
+            >
               Role
             </th>
-            <th scope="col" className="border px-4 py-2">
+            <th
+              scope="col"
+              className="px-4 py-2 border"
+            >
               Actions
             </th>
           </tr>
         </thead>
         <tbody className="text-neutral-dark">
           {usersData.map((user) => {
-            return <UserCard key={user.id} user={user} />;
+            return (
+              <UserCard
+                key={user.id}
+                user={user}
+              />
+            );
           })}
         </tbody>
       </table>
--- src/components/admin/PlayerCard.tsx
+++ src/components/admin/PlayerCard.tsx
@@ -1,31 +1,64 @@
 import { PlayerInfo } from "../../types/PlayerTypes";
 import Button from "../ui/Button";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 type PlayersListProps = {
   player: PlayerInfo;
 };
 
+const handleDelete = () => {
+  const confirmed = window.confirm("Are you sure you want to delete this user?");
+  if (confirmed) {
+    console.log(`User has been deleted.`);
+  }
+};
+
 export default function PlayerCard({ player }: PlayersListProps) {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate(`/admin/update-player/${player.id}`);
+  };
   return (
     <tr className="text-sm even:bg-neutral odd:bg-neutral-dark odd:text-white">
-      <td scope="col" className="px-4 py-2 border">
+      <td
+        scope="col"
+        className="px-4 py-2 border"
+      >
         {player.id}
       </td>
-      <td scope="col" className="px-4 py-2 border">
+      <td
+        scope="col"
+        className="px-4 py-2 border"
+      >
         {player.name}
       </td>
-      <td scope="col" className="px-4 py-2 border">
+      <td
+        scope="col"
+        className="px-4 py-2 border"
+      >
         {player.age}
       </td>
-      <td scope="col" className="px-4 py-2 border">
+      <td
+        scope="col"
+        className="px-4 py-2 border"
+      >
         {player.position}
       </td>
-      <td scope="col" className="flex justify-center gap-10 px-4 py-2 border">
-        <Link to={`/admin/update-player/${player.id}`}>
-          <Button label="Update" className="text-xs text-white bg-primary" />
-        </Link>
-        <Button label="Delete" className="text-xs text-white bg-primary" />
+      <td
+        scope="col"
+        className="flex justify-center gap-10 px-4 py-2 border"
+      >
+        <Button
+          onClick={handleNavigate}
+          label="Update"
+          className="text-xs text-white bg-primary"
+        />
+        <Button
+          type="button"
+          label="Delete"
+          className="text-xs text-white bg-primary"
+          onClick={handleDelete}
+        />
       </td>
     </tr>
   );
