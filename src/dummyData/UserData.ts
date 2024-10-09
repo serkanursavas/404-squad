0cd473c4cae2491c0dfeaf25008ac65c50f59c75
Caner Tanriverdi <canertanriverdi@Caners-MacBook-Air.local>
2024-10-09 20:50:25 +0300

    refac: formik added to user update page and validation and types added

--- src/dummyData/UserData.ts
+++ src/dummyData/UserData.ts
@@ -4,31 +4,31 @@ export const dummyUserInfo: UserInfo[] = [
   {
     id: 1,
     username: "Serkan",
-    role: "admin",
+    role: "ADMIN",
   },
   {
     id: 2,
     username: "Metehan",
-    role: "user",
+    role: "USER",
   },
   {
     id: 3,
     username: "Caner",
-    role: "user",
+    role: "USER",
   },
   {
     id: 4,
     username: "Isa Can",
-    role: "admin",
+    role: "ADMIN",
   },
   {
     id: 5,
     username: "Hayri Sencer",
-    role: "user",
+    role: "USER",
   },
   {
     id: 6,
     username: "Ahmet Furkan",
-    role: "user",
+    role: "USER",
   },
 ];
