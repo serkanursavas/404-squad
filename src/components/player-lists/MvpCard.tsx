566f3091c4da22b3f8fe05f2d6d08992b6bd0b18
serkanursavas <serkan.ursavas@icloud.com>
2024-10-15 01:40:16 +0300

    refactor: mvp card dynamic

--- src/components/player-lists/MvpCard.tsx
+++ src/components/player-lists/MvpCard.tsx
@@ -2,10 +2,17 @@ import Watermark from '../ui/Watermark'
 
 import mvpPic from '../../assets/images/mvp.png'
 import starIcon from '../../assets/icons/star.svg'
+import { PlayerInfo } from '../../types/PlayerTypes'
+import { useNavigate } from 'react-router-dom'
 
-export default function MvpCard() {
+export default function MvpCard({ player }: { player: PlayerInfo }) {
+  const navigate = useNavigate()
+
   return (
-    <div className="relative z-10 p-8 space-y-6 overflow-hidden bg-white">
+    <div
+      onClick={() => navigate(`/profile/${player.id}`)}
+      className="relative z-10 p-8 space-y-6 overflow-hidden bg-white cursor-pointer shadow-pixel"
+    >
       <Watermark
         watermarkIcon={starIcon}
         watermarksCount={15}
@@ -13,13 +20,15 @@ export default function MvpCard() {
         rotate
       />
       <div className="relative z-10 space-y-2 ">
-        <h2 className="text-xl ">Frenkie De Jong</h2>
-        <p className="text-[10px] text-neutral-dark">Attacking Midfielder</p>
+        <h2 className="text-xl ">
+          {player?.name} {player?.surname}
+        </h2>
+        <p className="text-[10px] text-neutral-dark">{player?.position}</p>
       </div>
       <div className="flex justify-end pr-10 ">
         <img
-          src={mvpPic}
-          className="z-10 w-56 "
+          src={mvpPic || player?.photo}
+          className="z-10 w-64 "
         />
       </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,9 @@ import Banner from '../components/match/Banner'
 import TopLists from '../components/player-lists/TopLists'
 
 import ballIcon from '../assets/icons/ball.png'
-import userIcon from '../assets/icons/user.svg'
 import ratingIcon from '../assets/icons/coin.svg'
 import MvpCard from '../components/player-lists/MvpCard'
+import { PlayerInfo } from '../types/PlayerTypes'
 
 const dummyMatch = {
   id: 1,
@@ -13,39 +13,50 @@ const dummyMatch = {
   homeTeamScore: 1,
   awayTeamScore: 2,
   dateTime: '2024-10-17T23:35:33.957',
-  played: true,
+  played: false,
   voted: false
 }
 
 const dummyPlayerData = [
   {
-    icon: userIcon,
+    playerId: 1,
     playerName: 'Serkan',
     statistic: '9'
   },
   {
-    icon: userIcon,
+    playerId: 2,
     playerName: 'Mete',
     statistic: '19'
   },
   {
-    icon: userIcon,
+    playerId: 3,
     playerName: 'Ismail',
     statistic: '2'
   },
   {
-    icon: userIcon,
+    playerId: 4,
     playerName: 'Isa',
     statistic: '7'
   }
 ]
+
+const player: PlayerInfo = {
+  id: 1,
+  name: 'John',
+  surname: 'Doe',
+  foot: 'Right',
+  photo: 'https://example.com/photo.jpg',
+  position: 'Forward',
+  active: true,
+  rating: 9.5
+}
 
 export default function Home() {
   return (
     <div className="space-y-6">
       <Banner match={dummyMatch} />
 
-      <MvpCard />
+      <MvpCard player={player} />
 
       <TopLists
         title="Top Scorer"
