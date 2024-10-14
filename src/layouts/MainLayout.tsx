89bff55b45625616e2a43b871b9be9c1df02382e
serkanursavas <serkan.ursavas@icloud.com>
2024-10-14 04:57:55 +0300

    refactor: improvements of admin home

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,27 +1,24 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/layout/Header";
-import Navigation from "../components/layout/Navigation";
-import { useState } from "react";
-import Footer from "../components/layout/Footer";
+import { Outlet } from 'react-router-dom'
+import Header from '../components/layout/Header'
+import Navigation from '../components/layout/Navigation'
+import { useState } from 'react'
+import Footer from '../components/layout/Footer'
 
 export default function MainLayout() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
   const toggleMobileMenu = () => {
-    setIsOpen((prevIsOpen) => !prevIsOpen);
-  };
+    setIsOpen(prevIsOpen => !prevIsOpen)
+  }
 
   return (
     <div className="flex flex-col min-h-screen bg-neutral">
-      <Header toggleMobileMenu={toggleMobileMenu} isOpen={isOpen} />
-      <main className="flex-grow p-2">
-        {isOpen ? (
-          <Navigation toggleMobileMenu={toggleMobileMenu} />
-        ) : (
-          <Outlet />
-        )}
-      </main>
+      <Header
+        toggleMobileMenu={toggleMobileMenu}
+        isOpen={isOpen}
+      />
+      <main className="flex-grow p-2">{isOpen ? <Navigation toggleMobileMenu={toggleMobileMenu} /> : <Outlet />}</main>
       <Footer />
     </div>
-  );
+  )
 }
