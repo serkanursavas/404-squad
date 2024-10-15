0d9e09a8a85fee68930a2aeff868521ea86bd428
serkanursavas <serkan.ursavas@icloud.com>
2024-10-16 01:41:39 +0300

    feat: adding toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { RouterProvider } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
 
 import routes from './router/routes'
+import { Provider } from 'react-redux'
+import { store } from './store'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Toast from './components/ui/Toast'
+
+const queryClient = new QueryClient()
 
 function App() {
-  return <RouterProvider router={routes} />
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Toast />
+        <RouterProvider router={routes} />
+      </QueryClientProvider>
+    </Provider>
+  )
 }
 
 export default App
