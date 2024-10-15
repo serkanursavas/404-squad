e08101466c824cfc099295ac6474119847b99323
serkanursavas <serkan.ursavas@icloud.com>
2024-10-15 14:43:24 +0300

    feat: create jwtutil and authservice

---
+++ src/services/authService.ts
@@ -0,0 +1,44 @@
+import { decodeToken } from '../utils/jwtDecode'
+import axiosInstance from './axiosInstance'
+
+interface LoginResponse {
+  token: string
+}
+
+interface User {
+  username: string
+  role: string
+}
+
+const login = async (username: string, password: string): Promise<{ user: User; token: string }> => {
+  const response = await axiosInstance.post<LoginResponse>('/login', {
+    username,
+    password
+  })
+
+  const { token } = response.data
+
+  localStorage.setItem('token', token)
+
+  const decodedToken = decodeToken(token)
+  if (!decodeToken) {
+    throw new Error('invalid token')
+  }
+
+  return {
+    user: {
+      username: decodedToken?.username || '',
+      role: decodedToken?.role || ''
+    },
+    token
+  }
+}
+
+const logout = (): void => {
+  localStorage.removeItem('token')
+}
+
+export default {
+  login,
+  logout
+}
--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,15 +1,16 @@
-import axios from 'axios'
+import axios, { InternalAxiosRequestConfig } from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http:..localhost:8080/api',
+  baseURL: 'http://localhost:8080/api',
   headers: {
     'Content-Type': 'application/json'
   }
 })
 
-axiosInstance.interceptors.request.use(config => {
+axiosInstance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem('token')
   if (token) {
+    config.headers = config.headers || {}
     config.headers['Authorization'] = `Bearer ${token}`
   }
   return config
