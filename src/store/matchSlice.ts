ee89af549125597fcdce9601d06c0cfb83c3750f
serkanursavas <serkan.ursavas@icloud.com>
2024-10-18 01:32:58 +0300

    feat: add fetchmatchdetail reducer

--- src/store/matchSlice.ts
+++ src/store/matchSlice.ts
@@ -20,9 +20,17 @@ const matchSlice = createSlice({
     },
     fetchAllMatchesSuccess: (state, action: PayloadAction<Match[]>) => {
       state.allMatches = [...state.allMatches, ...action.payload]
+    },
+    fetchMatchDetailsSuccess: (state, action: PayloadAction<Match>) => {
+      const matchIndex = state.allMatches.findIndex(match => match.id === action.payload.id)
+      if (matchIndex !== -1) {
+        state.allMatches[matchIndex] = action.payload
+      } else {
+        state.allMatches.push(action.payload)
+      }
     }
   }
 })
 
-export const { fetchNextMatchSuccess, fetchAllMatchesSuccess } = matchSlice.actions
+export const { fetchNextMatchSuccess, fetchAllMatchesSuccess, fetchMatchDetailsSuccess } = matchSlice.actions
 export default matchSlice.reducer
