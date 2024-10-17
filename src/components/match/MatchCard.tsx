9f65179178552bdf709d775f90e221fc0bd7f3f4
serkanursavas <serkan.ursavas@icloud.com>
2024-10-18 01:31:37 +0300

    refactor: change toastyfiy color and add class admin match card

--- src/components/match/MatchCard.tsx
+++ src/components/match/MatchCard.tsx
@@ -8,13 +8,17 @@ import { Match } from '../../services/matchService'
 type MatchInfoProps = {
   match: Match
   route: string
+  classname?: string
 }
 
-export default function MatchCard({ match, route }: MatchInfoProps) {
+export default function MatchCard({ match, route, classname = 'bg-white' }: MatchInfoProps) {
   const { date } = splitDateTime(match.dateTime)
 
   return (
-    <CardWrapper route={route}>
+    <CardWrapper
+      route={route}
+      classname={`${classname}`}
+    >
       <div className="text-right text-[10px] text-neutral-dark">{getFormattedDayAndMonth(date)}</div>
       <div className={`flex items-center justify-between ${match.id % 2 === 0 ? 'flex-row-reverse' : 'flex-row'}`}>
         <div className="flex items-center space-x-2">
