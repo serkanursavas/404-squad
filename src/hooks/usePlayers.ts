a71157f632ca58d6918920f7e9e6969190251e6d
serkanursavas <serkan.ursavas@icloud.com>
2024-10-18 17:58:03 +0300

    refactor: add active players logic

--- src/hooks/usePlayers.ts
+++ src/hooks/usePlayers.ts
@@ -1,26 +1,31 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { Player, UpdatePlayerData } from '../services/playerService'
 import playerService from '../services/playerService'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchPlayersSuccess } from '../store/playerSlice'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import { RootState } from '../store'
 
 const usePlayer = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
+  // Redux'tan oyuncu verilerini alıyoruz
+  const players = useSelector((state: RootState) => state.players.players)
+
   const { data, isLoading, isError, error } = useQuery<Player[], Error>({
     queryKey: ['players'],
-    queryFn: playerService.getAllPlayers
+    queryFn: playerService.getAllPlayers,
+    enabled: players.length === 0 // Eğer Redux'ta veri varsa, bu istek atılmaz
   })
 
   useEffect(() => {
     if (data) {
       dispatch(fetchPlayersSuccess({ players: data }))
     }
-  }, [data])
+  }, [data, dispatch])
 
   const { mutate: updatePlayer } = useMutation({
     mutationFn: ({ updatedData }: { updatedData: UpdatePlayerData }) => playerService.updatePlayer(updatedData),
@@ -33,7 +38,7 @@ const usePlayer = () => {
     }
   })
 
-  return { players: data, isLoading, isError, error, updatePlayer }
+  return { players: players.length > 0 ? players : data, isLoading, isError, error, updatePlayer }
 }
 
 export default usePlayer
