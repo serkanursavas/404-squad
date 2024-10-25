526cf846bedf7c755db3b8d87e1dd7bcfe0dfed9
serkanursavas <serkan.ursavas@icloud.com>
2024-10-25 18:31:14 +0300

    feat: adding top scorer service

--- src/services/goalService.ts
+++ src/services/goalService.ts
@@ -11,6 +11,13 @@ export interface GoalsUpdate {
   goals: Goal[]
 }
 
+export interface TopScorer {
+  id: number
+  name: string
+  surname: string
+  goalCount: number
+}
+
 const addGoals = async (goalsData: GoalsUpdate): Promise<any> => {
   try {
     const response = await axiosInstance.post('/goals/admin/addGoals', goalsData)
@@ -23,4 +30,16 @@ const addGoals = async (goalsData: GoalsUpdate): Promise<any> => {
   }
 }
 
-export default { addGoals }
+const getTopScorers = async (): Promise<TopScorer[]> => {
+  try {
+    const response = await axiosInstance.get('/goals/topScorers')
+    return response.data
+  } catch (error: any) {
+    const customError = new Error(error.response?.data?.message || error.message || 'An unknown error occurred') as CustomError
+    customError.status = error.response?.status || 500
+    customError.details = error.response?.data?.details || 'No additional details available'
+    throw customError
+  }
+}
+
+export default { addGoals, getTopScorers }
