1c92bbbb06ff15843435d692f541e337d51f549d
serkanursavas <serkan.ursavas@icloud.com>
2024-10-28 10:27:25 +0300

    refactor: filter active players

--- src/pages/AllPlayers.tsx
+++ src/pages/AllPlayers.tsx
@@ -5,13 +5,15 @@ import usePlayer from '../hooks/usePlayers'
 export default function AllPlayers() {
   const { players } = usePlayer()
 
+  const activePlayers = players?.filter(player => player.active)
+
   return (
     <div className="space-y-6">
       <TypingEffect
         text={['All Players']}
         className="text-sm text-purple-400 "
       />
-      <AllPlayersList playersData={players || []} />
+      <AllPlayersList playersData={activePlayers || []} />
     </div>
   )
 }
