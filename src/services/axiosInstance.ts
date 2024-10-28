d0e8f5644f79777c096ff97a8cc48894c73261de
serkanursavas <serkan.ursavas@icloud.com>
2024-10-28 15:28:58 +0300

    refactor: baseurl

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: 'https://squad-404-90b8d38132d9.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
