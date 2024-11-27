34320e012060271705bbf90cc9b95eded73024e7
serkanursavas <serkan.ursavas@icloud.com>
2024-11-27 21:40:53 +0300

    fix: service worker unregistiration

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,28 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Toast from './components/ui/Toast'
+import { useEffect } from 'react'
 
 const queryClient = new QueryClient()
 
+function cleanupServiceWorkers() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+      registrations.forEach(registration => {
+        registration.unregister().then(() => {
+          console.log('Service Worker unregistered:', registration)
+        })
+      })
+    })
+  }
+}
+
 function App() {
+  useEffect(() => {
+    // Service Worker temizleme işlemini yalnızca bir kez çalıştır
+    cleanupServiceWorkers()
+  }, [])
+
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
